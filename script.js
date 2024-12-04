91a5be92e173830ca206e1bb55742b56905e7031
Nelly <nelly.babillon@gmail.com>
2024-12-04 12:04:17 +0100

    EasytimerJS

--- script.js
+++ script.js
@@ -2,24 +2,7 @@ console.log('Merde')
 
 // Timer
 
-var timer = new Timer({
-     tick : 1,
-     ontick : function (sec) {
-          console.log('interval', sec);
-     },
-     onstart : function() {
-          console.log('timer started');
-     }
-     });
 
-     // defining options using on
-     timer.on('end', function () {
-     console.log('timer ended');
-     this.start(4).off('end');
-     });
-
-     //start timer for 10 seconds
-     timer.start(10);
 
 // Random Player
 
